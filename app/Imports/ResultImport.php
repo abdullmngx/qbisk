3b16959447e81f9401e7d6e5f16c1285d27b37a8
abdullmngx <abdullahimuhammad51@gmail.com>
2023-12-13 04:18:43 +0100

    fix error with result bulk upload

--- app/Imports/ResultImport.php
+++ app/Imports/ResultImport.php
@@ -20,10 +20,13 @@ class ResultImport implements ToModel, WithHeadingRow
     public function model(array $row)
     {
         $student = Student::where('admission_number', $row['admission_number'])->first();
-        $current_session = Configuration::where('name', 'current_session')->first()?->value;
-        $current_term = Configuration::where('name', 'current_term')->first()?->value;
-        $total_score = intval($row['ca1_score']) + intval($row['ca2_score']) + intval($row['test_score']) + intval($row['exam_score']);
-        $grade_id = GradeSetting::match(['section_id' => $student->section_id, 'form_id' => $student->form_id])->where('min_score', '<=', $total_score)->where('max_score', '>=', $total_score)->first()?->id;
-        return Result::updateOrCreate(['session_id' => $current_session, 'term_id' => $current_term, 'student_id' => $student->id, 'subject_id' => $this->subject_id], ['session_id' => $current_session, 'term_id' => $current_term, 'form_id' => $student->form_id, 'arm_id' => $student->arm_id, 'student_id' => $student->id, 'subject_id' => $this->subject_id, 'ca1_score' => intval($row['ca1_score']), 'ca2_score' => intval($row['ca2_score']), 'ca3_score' => intval($row['test_score']), 'exam_score' => intval($row['exam_score']), 'total_score' => $total_score, 'grade_id' => $grade_id]);
+        if (!is_null($student))
+        {
+            $current_session = Configuration::where('name', 'current_session')->first()?->value;
+            $current_term = Configuration::where('name', 'current_term')->first()?->value;
+            $total_score = intval($row['ca1_score']) + intval($row['ca2_score']) + intval($row['test_score']) + intval($row['exam_score']);
+            $grade_id = GradeSetting::match(['section_id' => $student->section_id, 'form_id' => $student->form_id])->where('min_score', '<=', $total_score)->where('max_score', '>=', $total_score)->first()?->id;
+            return Result::updateOrCreate(['session_id' => $current_session, 'term_id' => $current_term, 'student_id' => $student->id, 'subject_id' => $this->subject_id], ['session_id' => $current_session, 'term_id' => $current_term, 'form_id' => $student->form_id, 'arm_id' => $student->arm_id, 'student_id' => $student->id, 'subject_id' => $this->subject_id, 'ca1_score' => intval($row['ca1_score']), 'ca2_score' => intval($row['ca2_score']), 'ca3_score' => intval($row['test_score']), 'exam_score' => intval($row['exam_score']), 'total_score' => $total_score, 'grade_id' => $grade_id]);
+        }
     }
 }
